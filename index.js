e1f62e5cf7f519b19e16b01197ce7382129cab2d
boboiboy <merazhossain5662@gmail.com>
2026-07-29 01:33:38 +0600

    comment outed ping line

--- index.js
+++ index.js
@@ -300,7 +300,7 @@ async function run() {
       res.json(result);
     });
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
